ea902cb8d7e910cc522c8a8c705069823e56f59d
JBS Bento Silva7 <joasilva19577@gmail.com>
2026-08-21 22:43:33 -0300

    Modify app.run to read port from environment variable
    Updated app.run to use environment variable for port.

--- app.py
+++ app.py
@@ -299,7 +299,8 @@ def sair():
 with app.app_context():
     db.create_all()
 
-# RODAR NA PORTA 5000 — COMO PEDIU
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
  
