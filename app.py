0c103dacf4481ff23d26aaa0e1feb1326a9353c0
JBS Bento Silva7 <joasilva19577@gmail.com>
2026-08-28 10:13:04 -0300

    Update header comment format in app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
- # ==================================================
+# ==================================================
 # © 2026 JNB TECNOLOGIA — COM DOWNLOAD DE DNA ✅
 # REGRA OCULTA NO JOGO 🃏 · DNA COM CHAVE SECRETA 🔒
 # REDE · JOGO BENTINHO · JOGO CARTAS · IA · DNA
